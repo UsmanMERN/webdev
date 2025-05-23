b7fad37f542cca8765aef33a7344a07e8a26076a
UsmanMERN <usman853136@gmail.com>
2025-05-23 12:46:34 +0500

    added

---
+++ javascript/week1/day3/scripts/operations.js
@@ -0,0 +1,62 @@
+// operations
+// - Arithmetic Operators (`+` , `-` , `*` , `/` , `%` )
+// - Comparison Operators (`==` , `===` , `!=` , `!==` , `>` , `<` , `>=` , `<=` )
+// - Logical Operators (`&&` , `||` , `!` )
+// - Assignment Operators (`=` , `+=` , `-=` , `*=` , `/=` )
+
+
+// Arithmetic Operators
+
+
+// alert("Please enter your name");
+
+// let name = prompt("Enter your name");
+// // console.log(typeof );
+
+// console.log(+name);
+// console.log(typeof name);
+
+
+// document.getElementById("name").innerHTML = name;
+
+// alert(name);
+
+// let num1 = 25;
+// let num2 = 5;
+
+// let num3 = num1 % num2;
+
+// console.log('a :>> ', num3);
+
+
+// let num2 = 34
+// let num1 = 56
+
+// let num3 = num1 >= num2
+// console.log('num3 :>> ', num3 && num1 > num2);
+
+
+// console.log('num3 :>> ', true && false); // ==> false
+// console.log('num3 :>> ', false && false); // ==> false
+// console.log('num3 :>> ', true && true); // ==> true
+
+// console.log('num3 :>> ', true || false); // ==> true
+// console.log('num3 :>> ', false || false); // ==> false
+// console.log('num3 :>> ', true || true); // ==> true
+
+console.log('num3 :>> ', !true); // ==> false
+console.log('num4 :>> ', !false); // ==> true
+
+
+
+let name = 'Adnan';
+let age = 17;
+let city = 'lahore';
+let isStudent = false;
+
+
+// if (age >= 18 || city == 'faislabad' || isStudent) {
+//     console.log('You are eligible to vote');
+// } else {
+//     console.log('You are not eligible to vote');
+// }
--- javascript/week1/day3/scripts/script.js
+++ javascript/week1/day3/scripts/script.js
@@ -47,37 +47,37 @@ let myArray = [1, 2, 3, 4, "my name is usama", true, null, undefined, { name: "u
 
 // let myObject = { name: "usama", age: 20, city: "karachi", isStudent: true, isMarried: false, hobbies: ["reading", "writing", "coding"], address: { city: "karachi", country: "pakistan" }, dateOfBirth: new Date("2000-01-01"), id: Symbol("my name is usama") };
 
-let myObject = { name: "Azam", age: "24", city: "chokara" }
+// let myObject = { name: "Azam", age: "24", city: "chokara" }
 
-console.log('myObject :>> ', myObject.name);
+// console.log('myObject :>> ', myObject.name);
 
-const age = 122121
+// const age = 122121
 
-let isStudent = undefined;
+// let isStudent = undefined;
 
-let myTransactions = [200, 300, 400, 500, 600, 700, 800, 900, 1000]
+// let myTransactions = [200, 300, 400, 500, 600, 700, 800, 900, 1000]
 
-let myTransaction = {
-    transaction1: 200,
-    transaction2: 300,
-    transaction3: 400,
-    transaction4: 500,
-    transaction5: 600,
-    transaction6: 700,
-    transaction7: 800,
-    transaction8: 900,
-    transaction9: 1000
-}
+// let myTransaction = {
+//     transaction1: 200,
+//     transaction2: 300,
+//     transaction3: 400,
+//     transaction4: 500,
+//     transaction5: 600,
+//     transaction6: 700,
+//     transaction7: 800,
+//     transaction8: 900,
+//     transaction9: 1000
+// }
 
 
-function eidfuncationBBQ() {
-    console.log("cancel");
-    console.log("cancel");
-    console.log("cancel");
-    console.log("cancel");
-    console.log("cancel");
-}
-eidfuncationBBQ();
+// function eidfuncationBBQ() {
+//     console.log("cancel");
+//     console.log("cancel");
+//     console.log("cancel");
+//     console.log("cancel");
+//     console.log("cancel");
+// }
+// eidfuncationBBQ();
 
 
 // DataTypes
@@ -96,3 +96,151 @@ eidfuncationBBQ();
 // array = [1, 2, 3, 4, 5];
 // object = { name: 'usama', age: 20, city: 'karachi' };
 // function = function() { console.log('my name is usama'); };
+
+
+// mutable and immutable data types
+
+// mutable data types = array, object, function
+// immutable data types = string, number, boolean, undefined, null, BigInt
+// Immutable example (string)
+// let str = "hello";
+// let newStr = str.toUpperCase();
+// console.log(str); // Output: "hello"
+// console.log(newStr) // Output: "HELLO"
+
+
+
+// let name, age, city, isStudent, isMarried, hobbies, address, dateOfBirth, id;
+// name = "usama";
+// age = 20;
+// city = "karachi";
+// isStudent = true;
+
+// let subject = "Web Development";
+
+// let usamaSubject = subject.toLowerCase();
+
+// console.log('usamaSubject :>> ', usamaSubject);
+// console.log('subject :>> ', subject);
+
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const arr1 = arr
+
+// arr1.push(11, 12, 13, 14, 15);
+
+// arr.push(16, 17, 18, 19, 20);
+
+// console.log('arr :>> ', arr);
+// console.log('arr1 :>> ', arr1);
+
+
+// string to number conversion
+
+// let str = "456.789px";
+// // console.log('str :>> ', str);
+// let num1 = Number(str);     // 456
+// let num2 = parseInt(str);   // 456
+// let num3 = parseFloat(str); // 456
+
+// console.log('num1 :>> ', num1);
+// console.log('num2 :>> ', num2);
+// console.log('num3 :>> ', num3);
+
+// boolean to number conversion
+
+// let bool = true;
+// let num4 = Number(bool);    // 1
+
+// console.log('num4 :>> ', num4);
+
+
+
+// let str2 = "3.14";
+// let num5 = parseInt(str2);   // 3
+// let num6 = parseFloat(str2); // 3.14
+
+// let str3 = "10px";
+// let num9 = Number(str3)
+// let num7 = parseInt(str3);   // 10
+// let num8 = parseFloat(str3); // 10
+
+
+// Converting to a String
+
+// let num = 123;
+// let str1 = String(num);    // "123"
+// let str2 = num.toString(); // "123"
+
+// console.log('str1 :>> ', str1);
+// console.log('str2 :>> ', str2);
+// console.log('num :>> ', num);
+
+
+// let bool = true;
+// let str3 = String(bool);   // "true"
+// let str4 = bool.toString(); // "true"
+
+// console.log('str3 :>> ', str3);
+// console.log('str4 :>> ', str4);
+// console.log('bool :>> ', bool);
+
+
+// let arr = [1, 2, 3];
+// let str5 = String(arr);    // "1,2,3"
+// let str6 = arr.toString(); // "1,2,3"
+
+// console.log('str5 :>> ', str5);
+// console.log('str6 :>> ', str6);
+// console.log('arr :>> ', arr);
+
+// Converting to Boolean
+
+
+// let num = 100;
+// let bool1 = Boolean(num);   // true
+
+// console.log('bool1 :>> ', bool1);
+// let str = 1;
+// let bool2 = Boolean(str);   // false
+
+// if (str) {
+//     // document.writeln("Usama")
+//     console.log("usman shak");
+// } else {
+//     document.writeln("unkown")
+
+// }
+
+// console.log('bool2 :>> ', bool2);
+// let obj = {};
+// let bool3 = Boolean(obj);   // true
+
+// console.log('bool3 :>> ', bool3);
+
+// let nullVal = null;
+// let bool4 = Boolean(nullVal); // false
+// console.log('bool4 :>> ', bool4);
+
+
+
+// let name = "usama";
+
+// let newName = name.toUpperCase();
+
+// console.log('name :>> ', name);
+// console.log('newName :>> ', newName);
+
+
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+// let newArr = arr
+
+// newArr.push(11, 12, 13, 14, 15);
+// arr.push(11, 12, 13, 14, 15);
+
+// console.log('arr :>> ', arr);
+// console.log('newArr :>> ', newArr);
